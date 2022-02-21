91af28943a3ed734b2ba25b603bdb9642db2d052
Keno Bacunawa <renzpalonpon@gmail.com>
2022-02-22 03:09:21 +0800

    added class api and render list

--- src/views/teacher_dashboard/ClassDetail.vue
+++ src/views/teacher_dashboard/ClassDetail.vue
@@ -78,115 +78,55 @@
             </ion-col>
         </ion-row>
         <ion-list>
-           <ion-item>
-               <ion-grid class="ion-no-padding">
-                    <ion-row class="ion-justify-content-around">
-                        <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">Jone Da</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" color="success">present</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding">
-                            <ion-row class="ion-no-padding ion-justify-content-end">
+            <div v-if="!getting_attendance">
+                <ion-item v-for="attendance in attendances" :key="attendance.id">
+                    <ion-grid class="ion-no-padding">
+                            <ion-row class="ion-justify-content-around">
+                                <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">{{attendance.student.display_name_mobile}}</ion-label></ion-col>
+                                <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" :color="checkColorForRemark(attendance.attendance.remarks)">{{checkRemarkValue(attendance.attendance.remarks)}}</ion-label></ion-col>
                                 <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
-                                </ion-col>
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
+                                    <ion-row class="ion-no-padding ion-justify-content-end">
+                                        <ion-col size=4 class="ion-no-padding">
+                                            <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
+                                        </ion-col>
+                                        <ion-col size=4 class="ion-no-padding">
+                                            <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
+                                        </ion-col>
+                                    </ion-row>
                                 </ion-col>
                             </ion-row>
-                        </ion-col>
-                    </ion-row>
-               </ion-grid>
-          </ion-item>
+                    </ion-grid>
+                </ion-item>
+            </div>
+            <div v-else>
+                <ion-item v-for="rep in 5" :key="rep">
+                    <ion-grid class="ion-no-padding">
+                            <ion-row class="ion-justify-content-around">
+                                <ion-col size=4 class="ion-no-padding">
+                                    <ion-label class="student-name">
+                                        <ion-skeleton-text ></ion-skeleton-text>
 
-          <ion-item>
-               <ion-grid class="ion-no-padding">
-                    <ion-row class="ion-justify-content-around">
-                        <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">Abdul Kajul</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" color="success">present</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding">
-                            <ion-row class="ion-no-padding ion-justify-content-end">
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
+                                    </ion-label>
+                                </ion-col>
+                                <ion-col size=4 class="ion-no-padding ion-text-center">
+                                    <ion-label class="attendance-status" color="primary">
+                                        <ion-skeleton-text ></ion-skeleton-text>
+                                    </ion-label>
                                 </ion-col>
                                 <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
-                                </ion-col>
-                            </ion-row>
-                        </ion-col>
-                    </ion-row>
-               </ion-grid>
-          </ion-item>
-          <ion-item>
-               <ion-grid class="ion-no-padding">
-                    <ion-row class="ion-justify-content-around">
-                        <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">Salani Don</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" color="warning">late</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding">
-                            <ion-row class="ion-no-padding ion-justify-content-end">
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
-                                </ion-col>
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
+                                    <ion-row class="ion-no-padding ion-justify-content-end">
+                                        <ion-col size=4 class="ion-no-padding">
+                                            <ion-img src="/assets/img/edit_button.png" class="action-btn dark"></ion-img>
+                                        </ion-col>
+                                        <ion-col size=4 class="ion-no-padding">
+                                            <ion-img src="/assets/img/delete_btn.png" class="action-btn dark"></ion-img>
+                                        </ion-col>
+                                    </ion-row>
                                 </ion-col>
                             </ion-row>
-                        </ion-col>
-                    </ion-row>
-               </ion-grid>
-          </ion-item>
-          <ion-item>
-               <ion-grid class="ion-no-padding">
-                    <ion-row class="ion-justify-content-around">
-                        <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">Geta lon</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" color="success">present</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding">
-                            <ion-row class="ion-no-padding ion-justify-content-end">
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
-                                </ion-col>
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
-                                </ion-col>
-                            </ion-row>
-                        </ion-col>
-                    </ion-row>
-               </ion-grid>
-          </ion-item>
-          <ion-item>
-               <ion-grid class="ion-no-padding">
-                    <ion-row class="ion-justify-content-around">
-                        <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">Debal Iktad</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" color="danger">absent</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding">
-                            <ion-row class="ion-no-padding ion-justify-content-end">
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
-                                </ion-col>
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
-                                </ion-col>
-                            </ion-row>
-                        </ion-col>
-                    </ion-row>
-               </ion-grid>
-          </ion-item>
-        <ion-item>
-               <ion-grid class="ion-no-padding">
-                    <ion-row class="ion-justify-content-around">
-                        <ion-col size=4 class="ion-no-padding"><ion-label class="student-name">Debal Iktad</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding ion-text-center"><ion-label class="attendance-status" color="danger">absent</ion-label></ion-col>
-                        <ion-col size=4 class="ion-no-padding">
-                            <ion-row class="ion-no-padding ion-justify-content-end">
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/edit_button.png" class="action-btn"></ion-img>
-                                </ion-col>
-                                <ion-col size=4 class="ion-no-padding">
-                                    <ion-img src="/assets/img/delete_btn.png" class="action-btn"></ion-img>
-                                </ion-col>
-                            </ion-row>
-                        </ion-col>
-                    </ion-row>
-               </ion-grid>
-          </ion-item>
+                    </ion-grid>
+                </ion-item>
+            </div>
         </ion-list>
     </ion-page>
 </template>
@@ -208,6 +148,8 @@ export default {
         select_options : {
             cssClass : 'select-option-filter'
         },
+        attendances :[],
+        getting_attendance :false
     }),
     ionViewWillEnter() {
         this.initialize()
@@ -216,9 +158,19 @@ export default {
         this.initialize()
         this.getDaysFilter()
     },
+    watch : {
+        selected_day : {
+            handler(val) {
+                if(val){
+                    this.getAttendances()
+                }
+            }
+        }
+    },
     methods : {
         initialize(){
             this.getting_class = true
+            this.getting_attendance = true
             this.$axios.get('teacher/v1/class-detail/'+this.$route.params.id).then(({data}) => {
                 this.class_detail = data
                 this.getting_class = false
@@ -228,14 +180,50 @@ export default {
             this.$axios.get('teacher/v1/class-detail/'+this.$route.params.id+'/get-days-filter').then(({data}) => {
                 this.days = data
                 this.selected_day = this.dateFromDay(this.class_detail.schedule.day.toUpperCase())
+                this.getAttendances()
+
             })
+
         },
         getAttendances(){
-            
-            this.$axios.get('teacher/v1/class-detail/'+this.$route.params.id+'/get-attendances').then(({data}) => {
+            this.getting_attendance = true
+            let params = {
+                date_filter : this.selected_day
+            }
+            this.$axios.get('teacher/v1/class-detail/'+this.$route.params.id+'/get-attendances',{params}).then(({data}) => {
                 this.attendances = data
+                this.getting_attendance = false
             })
         },
+        checkRemarkValue(remark) {
+
+            switch(remark) {
+                case 0 :
+                    return 'Absent';
+                case 1 :
+                    return 'Present';
+                case 2 :
+                    return 'Late';
+                case 3:
+                    return 'Excused';
+                default :
+                    break;
+            }
+        },
+        checkColorForRemark(remark) {
+            switch(remark) {
+                case 0 :
+                    return 'danger';
+                case 1 :
+                    return 'success';
+                case 2 :
+                    return 'warning';
+                case 3:
+                    return 'info';
+                default :
+                    break;
+            }
+        }
     }
 }
 </script>
@@ -274,6 +262,9 @@ export default {
     .action-btn {
         width: 28px;
         height: 28px;
+    }
+    .action-btn.dark {
+        filter: brightness(44%);
     }
     ion-list {
         height: 32vh;
